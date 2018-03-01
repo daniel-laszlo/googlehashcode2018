02f7735b604b30c4706642d0094a5a8f974e0d9d
daniel-laszlo <laszlodaniel@icloud.com>
2018-03-01 20:29:40 +0000

    iterate on cars in Main

--- src/src/Main.java
+++ src/src/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileReader fileReader = new FileReader("a_example.in");
         fileReader.readFile();
-        State state = fileReader.getState();
-        for (int i = 0; i < state.getStepCount(); ++i) {
-			Matrix.calculateMatrix(state)
+        State state = FileReader.state;
+        for (int step = 0; step < state.getStepCount(); ++step) {
+        	Matrix.calculateMatrix(FileReader.cars, FileReader.rides, FileReader.state, step);
+        	for (int i = 0; i < FileReader.cars.size(); ++i) {
+        		FileReader.cars.get(i).iterate();
+			}
 		}
     }
 }
